38fda9d538726b730091086f16c17f68a73de0cc
Gabriel Franco <grocha815@gmail.com>
2023-06-29 03:20:44 -0300

    javascript para type novmanete.

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,4 +1,4 @@
-export function reportWebVitals(onPerfEntry) {
+export function reportWebVitals(onPerfEntry?: (...args: any[]) => void) {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import("web-vitals").then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
