16fe1ee6214e383c76af354518b954ce62fa8965
Gabriel Franco <grocha815@gmail.com>
2023-07-09 08:43:17 -0300

    Videos feito e responsivo.

--- src/pages/Videos.tsx
+++ src/pages/Videos.tsx
@@ -1,8 +1,32 @@
 import React, { FC } from "react";
 import "../styles/videos.css";
 
+const videos = [
+  "https://www.youtube.com/embed/iKBCVZqqooY",
+  "https://www.youtube.com/embed/7NK_JOkuSVY",
+  "https://www.youtube.com/embed/v2H4l9RpkwM",
+  "https://www.youtube.com/embed/kXYiU_JCYtU",
+  "https://www.youtube.com/embed/5qF_qbaWt3Q",
+  "https://www.youtube.com/embed/ysSxxIqKNN0",
+  "https://www.youtube.com/embed/0xyxtzD54rM",
+];
+
 const Videos: FC = () => {
-  return <div>My Component</div>;
+  return (
+    <div className="video-container">
+      {videos.map((video, index) => (
+        <iframe
+          key={index}
+          src={video}
+          title={`Video ${index}`}
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          className="video"
+        />
+      ))}
+    </div>
+  );
 };
 
 export default Videos;
